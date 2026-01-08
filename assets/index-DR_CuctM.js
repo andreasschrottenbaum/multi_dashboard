b3f93603e44ba5b7ad7fdcbca1e6a96c79cbb6fe
andreasschrottenbaum <andreasschrottenbaum@users.noreply.github.com>
2026-01-08 23:56:01 +0000

    deploy: a87d57907dcb979975f552f79c8816b15676a91e

--- assets/index-DR_CuctM.js
+++ assets/index-DR_CuctM.js
@@ -129,7 +129,7 @@ Error generating stack: `+i.message+`
       <header class="dashboard-header">
         <h1>Multi-Framework Dashboard</h1>
         <p>A TypeScript dashboard with widgets in Web Components, React, Vue, and Svelte</p>
-        <p>See the source code on <a href="https://github.com/andreasschrottenbaum/multi_dashboard/tree/main">GitHub</a>.</p>
+        <p>See the source code on <a target="_blank" href="https://github.com/andreasschrottenbaum/multi_dashboard/tree/main">GitHub</a>.</p>
         <button id="reset-all" class="reset-all">Reset all widgets</button>
       </header>
 
