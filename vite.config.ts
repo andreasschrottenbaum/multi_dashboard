25234877aa9e938d57aec024832b9237e867ab13
Andreas Schrottenbaum <andreas.schrottenbaum@gmail.com>
2026-01-08 23:48:14 +0100

    re-build gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import sveltePreprocess from 'svelte-preprocess'
 
 export default defineConfig({
+  base: '/multi_dashboard/',
   plugins: [
     react(),
     vue(),
